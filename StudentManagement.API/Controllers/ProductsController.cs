3c55b5c77104531f0ad17dbaa20b7fb0d2433604
pramodkumar02 <pramod.kumar@ideaqu.com>
2026-09-11 16:57:56 +0530

    Add validation to product POST action

--- StudentManagement.API/Controllers/ProductsController.cs
+++ StudentManagement.API/Controllers/ProductsController.cs
@@ -32,6 +32,16 @@ namespace StudentManagement.API.Controllers
         [HttpPost]
         public IActionResult Add(Product product)
         {
+            if (string.IsNullOrWhiteSpace(product.Name))
+            {
+                return BadRequest("Product name is required.");
+            }
+
+            if (product.Price <= 0)
+            {
+                return BadRequest("Product price must be greater than zero.");
+            }
+
             var created = _productService.Add(product);
             return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
         }
