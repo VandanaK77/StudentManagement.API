6c6b3c66bf41fdb25e07ef81d78915663aa84dac
pramodkumar02 <pramod.kumar@ideaqu.com>
2026-09-11 16:36:28 +0530

    Add CategoriesController GET endpoint

---
+++ StudentManagement.API/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.API.Models;
+
+namespace StudentManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new[]
+            {
+                new Category { Id = 1, Title = "Electronics" },
+                new Category { Id = 2, Title = "Accessories" }
+            });
+        }
+    }
+}
---
+++ StudentManagement.API/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentManagement.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+    }
+}
